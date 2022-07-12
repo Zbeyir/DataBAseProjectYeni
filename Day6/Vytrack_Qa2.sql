800d688d7a2171316889096e26d4ecc920a4af9f
zübeyir <zeynelzatari@gmail.com>
2022-07-12 16:26:28 +0200

    Day_6 Vytrack Qa2 is done

---
+++ Day6/Vytrack_Qa2.sql
@@ -0,0 +1,6 @@
+select first_name,last_name,email from orocrm_contact
+where last_name = 'Californian';
+
+select * from orocrm_contact_email;
+
+-- write the query to get first_name,last_name,email for Cherryl
